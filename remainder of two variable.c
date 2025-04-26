70f794308be0acd14034e0aa40d1536d780042af
meeranfarooq <meeranbhat24@gmail.com>
2025-04-26 16:29:50 +0530

    Create remainder of two variable.c

---
+++ remainder of two variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+int a,b;
+
+printf("Enter value of the first number:");
+scanf("%d",&a);
+
+printf("Enter value of the second number:");
+scanf("%d",&b);
+
+printf("%d",a%b);
+
+return 0;
+
